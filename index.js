ce57cd43dc3c2c08ca04ec6ddf2c2e76c5b18e36
no-relation <eddiec76@gmail.com>
2018-11-25 12:08:50 -0600

    Done.

--- index.js
+++ index.js
@@ -3,5 +3,21 @@ function getFirstSelector(selector){
 };
 
 function nestedTarget(target){
-    
+    const nest = document.querySelector("#nested")
+    return nest.querySelector(".target")
 };
+
+function increaseRankBy(n) {
+    const ranked = document.querySelectorAll('.ranked-list li')
+    ranked.forEach(function(item){
+        return item.innerText = parseInt(item.innerText) + parseInt(n)
+    })
+}
+
+function deepestChild(node){
+    if (typeof node.innerText === 'string'){
+        return node
+    } else {
+        return deepestChild(node.child[0])
+    }
+}
